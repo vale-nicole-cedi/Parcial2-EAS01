28df98fb8aa11cadad2b3b30de7f755ada06aa5a
Mario Carrillo <mario.carrillo@clearcheck.us>
2024-03-04 10:31:24 -0600

    finished EAS 01

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Parcial2-EAS02
+# Parcial2-EAS01
+
+Esta es la actividad sumativa #1 del Parcial 2.
+Trabaja de manera individual.
+Asegúrate que tu código compile en todo momento y guarda cambios constantemente.
+
+## Instrucciones
+
+1. Haz un fork de este repositorio a tu cuenta de GitHub con correo institucional.
+2. Clona el repositorio a tu computadora.
+3. Trabaja sobre el archivo jugadores.cpp
+4. Resuelve cada uno de los comentarios multilínea que se encuentran en el archivo jugadores.cpp
+5. En caso de tener dudas sobre las instrucciones pregunta al profesor.
+6. Al terminar, haz commit de tus cambios y súbelos a tu repositorio de GitHub.
+7. Cierra todas las pestañas de Visual Studio Code.
+8. Entrega el link a tu repositorio.
+
+### Notas
+- Puedes hacer uso de tus trabajos pasados así como de la documentación de C++
+- Puedes agregar las funciones que sientas necesarias para poder completar la actividad.
---
+++ jugadores.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>  // Función rand().
+#include <ctime>    // Función time() para generar una nueva seed aleatoria.
+using namespace std;
+
+#define log(x) cout << x << endl;
+
+class Jugador{
+    /*
+        Agrega propiedades para guardar la vida(número entero), ataque(número entero) y nombre del jugador.
+        Agrega una propiedad para guardar el jugador que lo atacó por última vez.
+    */
+    
+    Jugador(); 
+    Jugador(/* agrega parámetros para asignar hp, ataque y nombre */); 
+    
+    void atacar(/* agregar parámetros para atacar a otro jugador*/); 
+};
+
+int main()
+{
+    srand(time(0));  // Seed para generar valores aleatorios
+    
+    /*
+        Crear al menos 2 jugadores con diferentes nombres, vida y ataque. 
+        Los jugadores deben atacarse entre sí, tomando turnos, hasta que uno pierda toda su vida.
+        En cuanto la vida del jugador se reduzca a 0 el programa muestra al ganador y se termina. 
+        No importa quien ataca primero.
+    */
+
+    return 0;
+}
+
+Jugador::Jugador(){
+    /*
+        Asignar 100 a hp y 5 a attack como valores default.
+        Asignar 'No definido' al nombre como default.
+    */
+}
+
+Jugador::Jugador(/* Agrega parámetros para asignar hp, ataque y nombre*/){
+    /*
+        Asignar los parámetros recibidos a las propiedades del objeto.
+        No se pueden asignar valores negativos a hp y attack.
+        Máximo 200 hp y 20 attack
+    */
+}
+
+void Jugador::atacar(/* Agregar parámetros para atacar a otro Jugador */){
+    /*
+        El jugador atacado(el que recibes como parámetro) pierde HP igual al ataque del jugador que ataca.
+        Existe un 20% de probabilidad de que el ataque falle y no haga daño.
+        Se debe mostrar lo que pasó en cada ataque:
+        nombre1 atacó a nombre2 e hizo 5 de daño, vida restante de nombre2 = 95
+        nombre2 atacó a nombre1 pero fallo, vida restante de nombre1 = 100
+        El jugador que atacó debe quedar guardar en la propiedad atacadoPor del jugador atacado.
+    */
+    /*
+    Nota:
+        int x = rand() & 1000; asigna un valor aleatorio entre 0 y 999 a x;
+        Puedes usar esto para la probabilidad del ataque.
+    */
+}
